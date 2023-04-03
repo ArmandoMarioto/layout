2d22ab2d0c5c416e2ef9d42ce7209bc9a6997fb7
Armando Marioto <armando_marioto100@hotmail.com>
2023-04-03 17:35:00 -0300

    Explicando questão 5

--- resposta_questao_5.js
+++ resposta_questao_5.js
@@ -1,4 +1,8 @@
 function calcularPermutacoes(n) {
+    //para saber a quantidade de distintas de um grupo pode ficar
+    // basta fazer o fatorial desse numero
+    //percorro o for, fazendo o fatorial até chagar no ultimo numero
+    //assim obtendo o valor da quantidade de distintas
   let fatorial = 1;
   for (let i = 1; i <= n; i++) {
     fatorial *= i;
