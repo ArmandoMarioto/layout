58edfb7ea99dd88f4e39db248b2fb7196c43dfaa
Armando Marioto <armando_marioto100@hotmail.com>
2023-04-03 17:00:26 -0300

    explicando a questão 2

--- resposta_questao_2.js
+++ resposta_questao_2.js
@@ -1,14 +1,21 @@
 function verificaDuplicados(arr) {
+  //criei um array para guardar os repitidos
   let repetidos = [];
+  //criei um set para guardar os os unicos
   let unicos = new Set();
+  //forzinho para percorrer o array
   for (let i = 0; i < arr.length; i++) {
     let valor = arr[i];
+    //se o valor ja existe no array unico, quer dizer que o elemento é
+    //repitido, então dou um push no array de repitidos
+    //caso não tenha no array unico, eu dou um add adicionando esse valor no set uncio
     if (unicos.has(valor)) {
       repetidos.push(valor);
     } else {
       unicos.add(valor);
     }
   }
+  //por fim retorno o array com os casos repitidos.
   return repetidos;
 }
 
